ad3f3dfc7c93d080095320f92edbe76bb26f63ec
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-04-24 10:10:30 +0530

    ..

---
+++ frontend/src/components/user/ManageSheet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ManageSheet = () => {
+  return (
+    <div>ManageSheet</div>
+  )
+}
+
+export default ManageSheet
--- frontend/src/components/user/Sheet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sheet = () => {
-  return (
-    <div>Sheet</div>
-  )
-}
-
-export default Sheet
