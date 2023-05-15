0c877b0d7dbc84f75f1ecbcc3f45b7db5da33517
0011viveksingh <vivek738063@gmail.com>
2023-05-15 09:46:56 +0530

    ..

--- frontend/src/components/main/Home.js
+++ frontend/src/components/main/Home.js
@@ -4,17 +4,94 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <header>
-        <div className="row">
-          <div className="col-md-7">
+    <>
+  <title>Home Page</title>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  {/* Bootstrap CSS */}
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+  />
+  {/* Navbar */}
+  <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+    <a className="navbar-brand" href="#">
+      Brand Name
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-          </div>
-          <div className="col-md-5">
-            
-          </div>
-        </div>
-    </header>
-    
+
+  {/* Cover Page */}
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">Safe and secure data handling with DataSavvy</h1>
+      <p className="lead">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+      </p>
+    </div>
+  </div>
+
+  {/* Section */}
+  <section className="container my-5">
+    <div className="row">
+      <div className="col-md-4">
+        <h2>Section 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+          risus.
+        </p>
+      </div>
+      <div className="col-md-4">
+        <h2>Section 2</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+          risus.
+        </p>
+      </div>
+      <div className="col-md-4">
+        <h2>Section 3</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
+          risus.
+        </p>
+      </div>
+    </div>
+  </section>
+  
+
+  {/* Bootstrap JS */}
+</>
+
   </>
   
     
--- frontend/src/components/user/ManageTools.js
+++ frontend/src/components/user/ManageTools.js
@@ -2,7 +2,72 @@ import React from 'react'
 
 const AddNewTool = () => {
   return (
-    <div>AddNewTool</div>
+    <>
+    <title>Bootstrap Form</title>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {/* Bootstrap CSS */}
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    />
+    <style
+      dangerouslySetInnerHTML={{
+        __html:
+          "\n\t\tbody {\n\t\t\tbackground-color: #f8f9fa; /* Set the background color */\n\t\t}\n\t"
+      }}
+    />
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <form>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                placeholder="Enter description"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="data">Data:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="data"
+                placeholder="Enter data"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="formula">Formula:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="formula"
+                placeholder="Enter formula"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+    {/* Bootstrap JS */}
+  </>
+  
+
   )
 }
 
