971d836ee81846291e43f0140fd1ac4a99eccdb1
m-triple-m <triplem656@gmail.com>
2023-05-24 12:30:21 +0530

    ..

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,55 +1,60 @@
 const app_config = {
-    apiUrl: "http://localhost:5000",
-    title: "DataSavvy",
-    themeColor: "#00a2b7",
-    status: {
-      login: {
-        success: 200,
-        fail: 401,
+  apiUrl: "http://localhost:5000",
+  title: "DataSavvy",
+  themeColor: "#00a2b7",
+  status: {
+    login: {
+      success: 200,
+      fail: 401,
+    },
+  },
+
+  toolpack: {
+    mean: {
+      name: "Mean",
+      description: "mean",
+      icon: "mean",
+      type: "statistical",
+      inputs: [
+        {
+          name: "values",
+          type: "array",
+          description: "values",
+          placeholder: "Enter Range Here",
+          required: true,
+        },
+      ],
+      calc: (values) => {
+        console.log(values);
       },
     },
-
-    toolpack: {
-      mean: {
-        name: "Mean",
-        description: "mean",
-        icon: "mean",
-        type: "statistical",
-        inputs: [{
+    median: {
+      name: "median",
+      description: "median",
+      icon: "median",
+      type: "statistical",
+      inputs: [
+        {
           name: "values",
           type: "array",
           description: "values",
           placeholder: "Enter Range Here",
           required: true,
-        }],
-        calc: (values) => {
-          console.log(values);
-        }
+        },
+      ],
+      calc: (values) => {
+        console.log(values);
       },
-      median: {
-        name: "median",
-        description: "median",
-        icon: "median",
-        type: "statistical",
-        inputs: [{
-          name: "values",
-          type: "array",
-          description: "values",
-          placeholder: "Enter Range Here",
-          required: true,
-        }],
-        calc: (values) => {
-          console.log(values);
-        }
-      },
-      regression : {
-        name: "Regression",
-        description: "regression analysis",
-        icon: "",
-        type: "forecasting",
-        inputs: [{
+    },
+    regression: {
+      name: "Regression",
+      description: "regression analysis",
+      icon: "",
+      type: "forecasting",
+      inputs: [
+        {
           name: "X-Range",
-          section: 'Input',
+          section: "Input",
           type: "array",
           description: "independent variable",
           placeholder: "Enter X-Range Here",
@@ -57,19 +62,18 @@ const app_config = {
         },
         {
           name: "Y-Range",
-          section: 'Input',
+          section: "Input",
           type: "array",
           description: "independent variable",
           placeholder: "Enter Y-Range Here",
           required: true,
-        }
+        },
       ],
-        calc: (values) => {
-          console.log(values);
-        }
-      }
-    }
-  };
-  
-  export default app_config;
-  
+      calc: (values) => {
+        console.log(values);
+      },
+    },
+  },
+};
+
+export default app_config;
