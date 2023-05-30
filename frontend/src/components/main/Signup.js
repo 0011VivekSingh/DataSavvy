8db0d5fb697be73d991e781f9d87eea5db302c27
0011viveksingh <vivek738063@gmail.com>
2023-05-30 10:30:21 +0530

    ..

--- frontend/src/components/main/Signup.js
+++ frontend/src/components/main/Signup.js
@@ -92,7 +92,7 @@ const Signup = () => {
         <div className="container  h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-              <div className="card mt-5" style={{ borderTopRightRadius: 50 }}>
+              <div className="card mt-5" >
                 <div className="card-body p-5">
                   <h2 className="text-uppercase text-center mb-2">
                     Create your account
@@ -178,6 +178,7 @@ const Signup = () => {
                         <u>Login here</u>
                       </Link>
                     </p>
+                    
                   </form>
                 </div>
               </div>
@@ -185,6 +186,7 @@ const Signup = () => {
           </div>
         </div>
       </div>
+      <svg className="mt-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="0.7" d="M0,320L60,272C120,224,240,128,360,112C480,96,600,160,720,181.3C840,203,960,181,1080,149.3C1200,117,1320,75,1380,53.3L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
     </section>
   );
 };
